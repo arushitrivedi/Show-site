84b14be33121d43c483ca381c4148d4f3daad984
Arushi Trivedi <arushi.19417@knit.ac.in>
2023-09-04 15:45:51 +0530

    Initial Commit
    Uploaded the files.

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+function Navigation()
+{
+    return <nav className="container">        
+    <div className='logo'> <img src='/images/brand_logo.png' alt='Nike logo'/>
+    </div>
+      
+      <ul>
+        <li>MENU</li>
+        <li>LOCATION</li>
+        <li>ABOUT US</li>
+        <li>CONTACT</li>
+      </ul>
+      <button>LogIn</button>
+    </nav>
+}
+export default Navigation;
